854e1f968679f7586418bc6cde5d424165c135d1
Adrian Garcia Rojas <adrian_gar016@hotmail.es>
2020-10-08 15:14:45 -0500

    ejemplo de como imprimir directamente con PrintDocument en C#

---
+++ ImpresionConPrintDocument/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ImpresionConPrintDocument
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnImprime = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.btn_iTextSharp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Impresion";
+            // 
+            // btnImprime
+            // 
+            this.btnImprime.Location = new System.Drawing.Point(174, 22);
+            this.btnImprime.Name = "btnImprime";
+            this.btnImprime.Size = new System.Drawing.Size(97, 23);
+            this.btnImprime.TabIndex = 1;
+            this.btnImprime.Text = "PrintDocument";
+            this.btnImprime.UseVisualStyleBackColor = true;
+            this.btnImprime.Click += new System.EventHandler(this.btnImprime_Click);
+            // 
+            // btn_iTextSharp
+            // 
+            this.btn_iTextSharp.Location = new System.Drawing.Point(174, 51);
+            this.btn_iTextSharp.Name = "btn_iTextSharp";
+            this.btn_iTextSharp.Size = new System.Drawing.Size(97, 23);
+            this.btn_iTextSharp.TabIndex = 2;
+            this.btn_iTextSharp.Text = "Print_iTextSharp";
+            this.btn_iTextSharp.UseVisualStyleBackColor = true;
+            this.btn_iTextSharp.Click += new System.EventHandler(this.btn_ZenBarcode_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 94);
+            this.Controls.Add(this.btn_iTextSharp);
+            this.Controls.Add(this.btnImprime);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnImprime;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button btn_iTextSharp;
+    }
+}
+
---
+++ ImpresionConPrintDocument/Form1.cs
@@ -0,0 +1,82 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImpresionConPrintDocument
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImprime_Click(object sender, EventArgs e)
+        {
+            // crearemos el objeto printDocument1 y le colocaremos la configuracion para implementar el metodo Imprimir
+            printDocument1 = new PrintDocument();
+            PrinterSettings ps = new PrinterSettings();
+            printDocument1.PrinterSettings = ps;
+            printDocument1.PrintPage += Imprimir;
+            printDocument1.Print();
+        }
+
+        private void Imprimir(object sender, PrintPageEventArgs e)
+        {          
+            //constantes para formato
+            int ancho = 250;
+            int axisY = 20;
+            System.Drawing.Font font = new System.Drawing.Font("Arial", 11, FontStyle.Regular, GraphicsUnit.Point);
+
+            //texto
+            e.Graphics.DrawString("Texto del ticket", font, Brushes.Black, new RectangleF(0, axisY+=20, ancho, 20));
+            e.Graphics.DrawString("Texto del ticket2", font, Brushes.Black, new RectangleF(0, axisY+=20, ancho, 20));
+
+            //recuepraciond de directorio de la imagen QR
+            string pathBinIn = Directory.GetCurrentDirectory();
+            string pathBin = Directory.GetParent(pathBinIn).ToString();
+            string pathProject = Directory.GetParent(pathBin).ToString();
+            System.Drawing.Image img = System.Drawing.Image.FromFile(pathProject + "/Images/ejemploQR1.png");
+            e.Graphics.DrawImage(img, new System.Drawing.Rectangle(0, axisY += 20, 150, 150));
+        }
+
+        
+        private void btn_ZenBarcode_Click(object sender, EventArgs e)
+        {
+            printDocument1 = new PrintDocument();
+            PrinterSettings ps = new PrinterSettings();
+            printDocument1.PrinterSettings = ps;
+            printDocument1.PrintPage += ImprimirZenBarcode;
+            printDocument1.Print();
+        }
+
+        private void ImprimirZenBarcode(object sender, PrintPageEventArgs e)
+        {
+            System.Drawing.Image imgZenBarcode;         
+            Zen.Barcode.CodeQrBarcodeDraw qrCode = Zen.Barcode.BarcodeDrawFactory.CodeQr;           
+            imgZenBarcode = qrCode.Draw("|MAPR860101SL1|3017169|1|200|ROX|01/01/2011 09:05:37|31/12/2012|", 1);
+
+            //constantes para formato
+            int ancho = 250;
+            int axisY = 20;
+            System.Drawing.Font font = new System.Drawing.Font("Arial", 11, FontStyle.Regular, GraphicsUnit.Point);
+
+            //texto
+            e.Graphics.DrawString("Texto del ticket", font, Brushes.Black, new RectangleF(0, axisY += 20, ancho, 20));
+            e.Graphics.DrawString("Texto del ticket2", font, Brushes.Black, new RectangleF(0, axisY += 20, ancho, 20));
+            e.Graphics.DrawImage(imgZenBarcode, new System.Drawing.Rectangle(0, axisY += 20, 75, 75));
+        }
+
+    }
+}
